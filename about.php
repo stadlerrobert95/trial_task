1efb996c91d51ca40dd9b2dd8b3b0745676f568d
Stadler Róbert <h987641@stud.u-szeged.hu>
2023-03-31 19:07:28 +0200

    initial

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;900&display=swap" rel="stylesheet">
+
+    <title>Rólunk</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+
+<section class="search-form">
+    <form action="" method="post">
+        <div class="search-header">
+            <p>Keresés a felhasználók között: </p>
+            <p class="hint">(felhasználónévben benne van a keresett szövegrészlet)</p>
+
+            <input type="text" name="search" placeholder="keresés.." class="box">
+            <input type="submit" name="submit" value="Keresés" class="btn">
+        </div>
+    </form>
+</section>
+
+<section class="result">
+    <div class="box-container">
+        <!-- Keresés -->
+
+    </div>
+</section>
+<div class="link">
+    <a href="books.php">Könyvek |</a><a href="dummy_genre.php">Műfaj |</a><a href="dummy_author.php">Szerzők</a>
+</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;900&display=swap" rel="stylesheet">
+
+    <title>Főoldal</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<button onclick="location.href='about.php'" type="button" class="menu">Rólunk</button>
+<button class="menu radius">Referenciáink</button>
+
+</body>
+
+</html>
